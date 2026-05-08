a725ea5e646538cd9e822c7fe49c6ebaaad04b85
sklyarov144-glitch <sklyarov144@gmail.com>
2026-05-08 18:34:34 +0300

    Use refined Yandex app

--- src/main.jsx
+++ src/main.jsx
@@ -1,7 +1,7 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
-import App from './AppYandex.jsx'
+import App from './AppYandexRefined.jsx'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
